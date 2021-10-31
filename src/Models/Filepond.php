83b0d54560b3da09cc94ffb4622893f671118bc9
nkudry <47333605+nkudry@users.noreply.github.com>
2021-11-01 01:19:33 +0200

    Update Filepond.php

--- src/Models/Filepond.php
+++ src/Models/Filepond.php
@@ -3,13 +3,12 @@
 namespace RahulHaque\Filepond\Models;
 
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Filepond extends Model
 {
-    use HasFactory, SoftDeletes;
+    use SoftDeletes;
 
     protected $guarded = [];
 
